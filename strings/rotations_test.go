f1bce67745fd5caf1e2557403ff87b1e1b0a2aea
Matheus Lima <mellomatheuslima@gmail.com>
2025-12-19 16:04:49 -0300

    Add TestCompareIsNonTrivialRotationAlgorithms

--- strings/rotations_test.go
+++ strings/rotations_test.go
@@ -1,6 +1,9 @@
 package strings
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 func TestIsNonTrivialRotation(t *testing.T) {
 	for _, test := range nonTrivialRotationTests {
@@ -16,6 +19,22 @@ func TestIsNonTrivialRotationIterative(t *testing.T) {
 		actual := IsNonTrivialRotationIterative(test.s1, test.s2)
 		if actual != test.expected {
 			t.Errorf("test '%s' failed: s1 '%s' s2 '%s', expected '%t', got '%t'", test.name, test.s1, test.s2, test.expected, actual)
+		}
+	}
+}
+
+func TestCompareIsNonTrivialRotationAlgorithms(t *testing.T) {
+	for _, test := range nonTrivialRotationTests {
+		start := time.Now() // Capture the start time
+		actual := IsNonTrivialRotation(test.s1, test.s2)
+		elapsed := time.Since(start) // Calculate the elapsed time
+		t.Logf("IsNonTrivialRotation :: Processing time: %s\n", elapsed)
+		start = time.Now() // Capture the start time
+		actualI := IsNonTrivialRotationIterative(test.s1, test.s2)
+		elapsed = time.Since(start) // Calculate the elapsed time
+		t.Logf("IsNonTrivialRotationIterative :: Processing time: %s\n", elapsed)
+		if actual != actualI {
+			t.Errorf("test '%s' failed: s1 '%s' s2 '%s', IsNonTrivialRotation got '%t', IsNonTrivialRotationIterative got '%t'", test.name, test.s1, test.s2, actual, actualI)
 		}
 	}
 }
